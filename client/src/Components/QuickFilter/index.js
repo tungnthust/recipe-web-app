bc0a3bc74e2f586631a95b83efd8733606da2c1b
tuan-cpu <tuan.lq184323@sis.hust.edu.vn>
2021-11-01 07:26:53 +0700

    initial QuickFilter

---
+++ client/src/Components/QuickFilter/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const QuickFilter = () =>{
+    return(
+        <div className="filter_container">
+            <div className="filter_title">
+                
+            </div>
+        </div>
+    )
+}
+
+export default QuickFilter;
