8fe79eb27cc293fede5f575dc7c318cd2ebdc3e5
Tien Nguyen <tien.nd184311@sis.hust.edu.vn>
2021-11-06 01:41:10 +0800

    + add footer part

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar1 from './Components/Navbar1';
 import QuickFilter from './Components/QuickFilter';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Navbar1/>
       </Router>
       <QuickFilter/>
+      
+      <Footer/>
     </div>
   );
 }
