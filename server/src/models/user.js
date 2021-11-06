c4a1e7e281775ed56f9dbdecaeecaf55dcc24349
hoangha0o0 <79790322+hoangha0o0@users.noreply.github.com>
2021-11-06 21:22:09 +0700

    #8 add user and ingredient model

---
+++ server/src/models/user.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const Task = require('./task')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }],
+    numOfFavourite: {
+        type: Number,
+        default: 0
+    },
+    favourited_recipes: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Recipe'
+    }],
+    avatar: {
+        type: Buffer
+    }
+}, {
+    timestamps: true
+})
+
+userSchema.virtual('own_recipe', {
+    ref: 'Recipe',
+    localField: '_id',
+    foreignField: 'author'
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
