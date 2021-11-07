837a064d01cd8fec56d09c9155d7b45b3664d577
thu1404 <thulk21cht@gmail.com>
2021-11-07 23:19:50 +0700

    navbar2

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar1 from './Components/Navbar1';
 import SlideShow from './Components/SlideShow';
+import Navbar2 from './Components/Navbar2';
 import QuickFilter from './Components/QuickFilter';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Navbar1/>
       </Router>
       <SlideShow/>
+      <Navbar2/>
       <QuickFilter/>
     </div>
   );
