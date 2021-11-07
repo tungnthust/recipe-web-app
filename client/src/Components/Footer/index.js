6ad0cbaae12aad65f95c3c7d512959389693a412
Tien Nguyen <tien.nd184311@sis.hust.edu.vn>
2021-11-07 21:25:52 +0800

    - edit font size of text,icon, fixed footer

--- client/src/Components/Footer/index.js
+++ client/src/Components/Footer/index.js
@@ -6,32 +6,43 @@ import {FaFacebook, FaGooglePlus, FaInstagram, FaTwitter, FaYoutube} from 'react
 const Footer = () =>{
 
     return(
-        <div className="App-footer">
+        <div className="App-footer" style={styleFixedBottom}>
             <div className="footer-container">
                 <div className="footer-content flex flex-jc-sb flex-ai-c">
                 <h4>Food Recipe Website - Group 10 - MoSD</h4>
                 <ul className="flex flex-jc-c flex-ai-c">
-                    <li><a href="https://www.facebook.com/ha.hoangthuy.9" target="_blank" className="footer-link"><i className="fa fa-facebook" aria-hidden="false">
+                    <li><a href="https://www.facebook.com" target="_blank" className="footer-link"><i className="fa fa-facebook" aria-hidden="false">
                             <FaFacebook/>
                     </i></a></li>
-                    <li><a href="https://www.facebook.com/ha.hoangthuy.9" target="_blank" className="footer-link"><i className="fa fa-google-plus" aria-hidden="true">
+                    <li><a href="https://www.google.com/" target="_blank" className="footer-link"><i className="fa fa-google-plus" aria-hidden="true">
                         <FaGooglePlus/>
                     </i></a></li>
-                    <li><a href="https://www.facebook.com/ha.hoangthuy.9" target="_blank" className="footer-link"><i className="fa fa-instagram" aria-hidden="true">
+                    <li><a href="https://www.instagram.com/" target="_blank" className="footer-link"><i className="fa fa-instagram" aria-hidden="true">
                         <FaInstagram/>
                     </i></a></li>
-                    <li><a href="https://www.facebook.com/ha.hoangthuy.9" target="_blank" className="footer-link"><i className="fa fa-twitter" aria-hidden="true">
+                    <li><a href="https://twitter.com/?lang=vi" target="_blank" className="footer-link"><i className="fa fa-twitter" aria-hidden="true">
                         <FaTwitter/>
                     </i></a></li>
-                    <li><a href="https://www.facebook.com/ha.hoangthuy.9" target="_blank" className="footer-link"><i className="fa fa-youtube" aria-hidden="true">
+                    <li><a href="https://www.youtube.com/" target="_blank" className="footer-link"><i className="fa fa-youtube" aria-hidden="true">
                         <FaYoutube/>
                     </i></a></li>
                 </ul>
                 </div>
             </div>
+
+           
         </div>
 
     )
 }
+
+
+var styleFixedBottom = {
+    position: "fixed",
+    left: "0",
+    bottom: "0",
+    height: "auto",
+    width: "100%",
+  }
 
 export default Footer;
