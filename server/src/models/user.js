a7020dc608947c21e9b680d495c3e69402a9815d
tungnthust <thanhtungbka1@gmail.com>
2021-11-10 22:57:55 +0700

    #8 validate password strength

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -35,7 +35,7 @@ const userSchema = new mongoose.Schema({
             if (value.toLowerCase().includes('password')) {
                 throw new Error('Password cannot contain "password"')
             }
-            criteria = /^(?=.*\d)(?=.*[a-z]).{6,20}$/
+            criteria = /^(?=.*\d)(?=.*[a-z])/
             if (!value.match(criteria)) {
                 throw new Error('Password must contain both character and digit.')
             }
