f773d90ce1fa62c1548f7711d23a6abc52933498
tuan-cpu <tuan.lq184323@sis.hust.edu.vn>
2021-11-13 10:29:02 +0700

    code organizing

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,10 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar1 from './Components/Navbar1';
-import SlideShow from './Components/SlideShow';
-import Navbar2 from './Components/Navbar2';
-import QuickFilter from './Components/QuickFilter';
-import Footer from './Components/Footer';
+import HomePage from './Components/HomePage';
 
 function App() {
   return (
     <div>
-      <Router>
-        <Navbar1/>
-      </Router>
-      <SlideShow/>
-      <Navbar2/>
-      <QuickFilter/>
-      <Footer/>
+      <HomePage/>
     </div>
   );
 }
