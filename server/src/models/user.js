81a253a31b388a9af40dcaa6f3c3b43e3dcf0e80
ntttrung <trung.ntt1210@gmail.com>
2021-11-20 10:19:33 +0700

    add login API

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
-
-
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+ 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
         trim: true
     },
-    username: {
+    username: { 
         type: String,
         required: true,
         trim: true,
@@ -78,6 +79,41 @@ userSchema.method.toJSON = function() {
     return userObject
 }
 
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'oneloveonefuture')
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
+
+
+userSchema.statics.findByCredentials = async(username, password) => {
+    const user = await User.findOne({username})
+
+    if(!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+//Hash the plain text
+userSchema.pre('save', async function (next) {
+    const user = this
+    if(user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next()
+})
 
 
 const User = mongoose.model('User', userSchema)
--- server/src/routers/user.js
+++ server/src/routers/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
 
-router.post('/users', async (req, res) =>{
+router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
@@ -10,6 +10,18 @@ router.post('/users', async (req, res) =>{
         res.status(201).send({user})
     } catch(e){
         res.status(400).send(e)
+    }
+})
+
+
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.username, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
     }
 })
 
