13ea790da3a00b88b418e6fc24006d3a2da2a1f5
Tien Nguyen <tien.nd184311@sis.hust.edu.vn>
2021-12-01 22:25:33 +0700

    aa

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 import SignInPage from './Pages/SignIn';
 import HomePage from './Pages/HomePage';
+import MembersPage from './Pages/MemberPage';
+import AuthorPage from './Pages/MemberPage/Author';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <SignInPage/>
-      {/* <HomePage/> */}
+      {/* <SignInPage/>
+      <HomePage/> */}
+      {/* <AuthorPage2/> */}
+      {/* <Route path='/' element={<HomePage/>}/>
+      <Route path='/members' element={<MembersPage/>}/> */}
+      {/* <AuthorPage/> */}
+
+
+    <Router>
+        <Routes>
+        
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/members' element={<MembersPage/>}/>
+          <Route path='/members/author1' element={<AuthorPage/>}/>
+      </Routes>
+    </Router>
     </div>
   );
 }
