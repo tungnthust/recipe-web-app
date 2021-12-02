6a79d3f8f944ed41b914dcfce255287ae5360d32
tuan-cpu <tuan.lq184323@sis.hust.edu.vn>
2021-12-02 15:59:46 +0700

    "fix"

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Route path='/signin' element={<SignInPage/>}/>
         <Route path='/signup' element={<SignUpPage/>}/>
         <Route path='/' element={<HomePage/>}/>
-        <Route path='/generalrecipes' element={<GeneralRecipes/>}/>
+        <Route path='/recipes' element={<GeneralRecipes/>}/>
         <Route path='/contact' element={<ContactPage/>}/>
       </Routes>
     </Router>
