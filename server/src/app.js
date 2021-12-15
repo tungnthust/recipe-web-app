7addd3773a22cecda4c38605b493a0405e3334b8
tungnthust <thanhtungbka1@gmail.com>
2021-12-15 22:56:40 +0700

    fix bug

--- server/src/app.js
+++
@@ -1,8 +0,0 @@
-require('./db/mongoose')
-const express = require('express')
-const userRouter = require('./routers/user')
-const app = express()
-
-app.use(express.json())
-app.use(userRouter)
-module.exports = app
