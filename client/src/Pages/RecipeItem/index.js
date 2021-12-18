0335cc1ca77ebb80bc6564ca7aaa36e2cb5293e6
haraAnh <anh.lh1101@gmail.com>
2021-12-18 20:32:14 +0700

    add footer to recipeItem

--- client/src/Pages/RecipeItem/index.js
+++ client/src/Pages/RecipeItem/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar1 from '../../Components/Navbar1';
 import Navbar2 from '../../Components/Navbar2';
 import Item from "./Item";
+import Footer from "../../Components/Footer";
 const RecipeItem = () =>{
     return(
         <div>
             <Navbar1/>
             <Navbar2/>
             <Item/>
+            <Footer/>
         </div>
     )
 }
