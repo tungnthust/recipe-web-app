823f481029a49a038264417b5769307f2cae48ea
thu1404 <thulk21cht@gmail.com>
2021-12-18 23:58:18 +0700

    fixnavbar

--- client/src/Pages/Recipe/recipeItems/index.js
+++ client/src/Pages/Recipe/recipeItems/index.js
@@ -3,7 +3,29 @@ import './index.css';
 import { FaTable,FaUsers, FaClock } from "react-icons/fa";
 
 const RecipeItems = (props) =>{
-  const {list} = props;
+  
+  const recipes =[
+    {
+      img_link: 'https://caresspet.com/wp-content/uploads/2018/10/s15-1024x640.jpg',
+      recipe_name: 'burger',
+      decription:'burger sandwith',
+      author_avatar:'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png',
+      author_name:'Hoang Thu',
+      difficulty:'3',
+      time:'30min'
+    },
+    {
+      img_link: 'https://www.hongkongfoodietours.com/wp-content/uploads/2019/01/Pineapple-bun-optimized-768x512.jpg',
+      recipe_name: 'Pineapple Buns',
+      decription:'Pineapple Buns made from pineapple',
+      author_avatar:'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png',
+      author_name:'Tuan',
+      difficulty:'3',
+      time:'50min'
+    }
+  ]
+  let recipelist = recipes.map(recipe => <li img_link={recipes.img_link}  />
+  )
   return (
     <section id="recipeItems">
       <div class="container">
@@ -11,261 +33,32 @@ const RecipeItems = (props) =>{
           <h1>Top Recipes </h1>
         </div>
         <div class='row'>
-          <div class='col-md-4'>
-            <div class='card-text-center'>
-            <div class='img-container'>
-                <img src='https://caresspet.com/wp-content/uploads/2018/10/s15-1024x640.jpg' class='card-img-top'></img>
-              </div>
-              <div class='card-body'>
-                <h5 class='card-title'>recipe name</h5>
-                <p class='card-text'>description</p>
-                <div class='author'>
-                  <img class='avatar' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png' alt='authorAvatar '></img>
-                  By 
-                  <p> authorName</p>
-                </div>
-              </div>
-              <div class='card-footer'>
-                <div class='card-footer-content'>
-                  <ul>
-                    <li title='difficulty'>difficulty </li>
-                    
-                    <li title='time'><FaClock/> time</li>
-                  </ul>
-                </div>
-              </div>
+        <div class='col-md-4'>
+        <div class='card-text-center'>
+        <div class='img-container'>
+            <img src={recipes.img_link} class='card-img-top'></img>
+          </div>
+          <div class='card-body'>
+            <h5 class='card-title'>{recipes.recipe_name}</h5>
+            <p class='card-text'>description</p>
+            <div class='author'>
+              <img class='avatar' src="{recipes.author_avatar}" alt='authorAvatar '></img>
+              By 
+              <p> {recipes.author_name}</p>
             </div>
           </div>
-          <div class='col-md-4'>
-            <div class='card-text-center'>
-            <div class='img-container'>
-                <img src='https://caresspet.com/wp-content/uploads/2018/10/s15-1024x640.jpg' class='card-img-top'></img>
-              </div>
-              <div class='card-body'>
-                <h5 class='card-title'>recipe name</h5>
-                <p class='card-text'>description</p>
-                <div class='author'>
-                  <img class='avatar' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png' alt='authorAvatar '></img>
-                  By 
-                  <p> authorName</p>
-                </div>
-              </div>
-              <div class='card-footer'>
-                <div class='card-footer-content'>
-                  <ul>
-                    <li title='difficulty'>difficulty </li>
-                    
-                    <li title='time'><FaClock/> time</li>
-                  </ul>
-                </div>
-              </div>
+          <div class='card-footer'>
+            <div class='card-footer-content'>
+              <ul>
+                <li title='difficulty'>{recipes.difficulty} </li>
+              
+                
+                <li title='time'><FaClock/> {recipes.time}</li>
+              </ul>
             </div>
           </div>
-          <div class='col-md-4'>
-            <div class='card-text-center'>
-            <div class='img-container'>
-                <img src='https://caresspet.com/wp-content/uploads/2018/10/s15-1024x640.jpg' class='card-img-top'></img>
-              </div>
-              <div class='card-body'>
-                <h5 class='card-title'>recipe name</h5>
-                <p class='card-text'>description</p>
-                <div class='author'>
-                  <img class='avatar' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png' alt='authorAvatar '></img>
-                  By 
-                  <p> authorName</p>
-                </div>
-              </div>
-              <div class='card-footer'>
-                <div class='card-footer-content'>
-                  <ul>
-                    <li title='difficulty'>difficulty </li>
-                    
-                    <li title='time'><FaClock/> time</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class='col-md-4'>
-            <div class='card-text-center'>
-            <div class='img-container'>
-                <img src='https://caresspet.com/wp-content/uploads/2018/10/s15-1024x640.jpg' class='card-img-top'></img>
-              </div>
-              <div class='card-body'>
-                <h5 class='card-title'>recipe name</h5>
-                <p class='card-text'>description</p>
-                <div class='author'>
-                  <img class='avatar' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png' alt='authorAvatar '></img>
-                  By 
-                  <p> authorName</p>
-                </div>
-              </div>
-              <div class='card-footer'>
-                <div class='card-footer-content'>
-                  <ul>
-                    <li title='difficulty'>difficulty </li>
-                    
-                    <li title='time'><FaClock/> time</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class='col-md-4'>
-            <div class='card-text-center'>
-            <div class='img-container'>
-                <img src='https://caresspet.com/wp-content/uploads/2018/10/s15-1024x640.jpg' class='card-img-top'></img>
-              </div>
-              <div class='card-body'>
-                <h5 class='card-title'>recipe name</h5>
-                <p class='card-text'>description</p>
-                <div class='author'>
-                  <img class='avatar' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png' alt='authorAvatar '></img>
-                  By 
-                  <p> authorName</p>
-                </div>
-              </div>
-              <div class='card-footer'>
-                <div class='card-footer-content'>
-                  <ul>
-                    <li title='difficulty'>difficulty </li>
-                    
-                    <li title='time'><FaClock/> time</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class='col-md-4'>
-            <div class='card-text-center'>
-            <div class='img-container'>
-                <img src='https://caresspet.com/wp-content/uploads/2018/10/s15-1024x640.jpg' class='card-img-top'></img>
-              </div>
-              <div class='card-body'>
-                <h5 class='card-title'>recipe name</h5>
-                <p class='card-text'>description</p>
-                <div class='author'>
-                  <img class='avatar' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png' alt='authorAvatar '></img>
-                  By 
-                  <p> authorName</p>
-                </div>
-              </div>
-              <div class='card-footer'>
-                <div class='card-footer-content'>
-                  <ul>
-                    <li title='difficulty'>difficulty </li>
-                    
-                    <li title='time'><FaClock/> time</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class='col-md-4'>
-            <div class='card-text-center'>
-            <div class='img-container'>
-                <img src='https://caresspet.com/wp-content/uploads/2018/10/s15-1024x640.jpg' class='card-img-top'></img>
-              </div>
-              <div class='card-body'>
-                <h5 class='card-title'>recipe name</h5>
-                <p class='card-text'>description</p>
-                <div class='author'>
-                  <img class='avatar' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png' alt='authorAvatar '></img>
-                    By
-                    <p> authorName</p>
-                  
-                </div>
-              </div>
-              <div class='card-footer'>
-                <div class='card-footer-content'>
-                  <ul>
-                    <li title='difficulty'>difficulty </li>
-                    <li title='yeild'><FaTable/> Yeild </li>
-                    <li title='number of people'><FaUsers/> number ppl </li>
-                    <li title='time'><FaClock/> time</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class='col-md-4'>
-            <div class='card-text-center'>
-            <div class='img-container'>
-                <img src='https://caresspet.com/wp-content/uploads/2018/10/s15-1024x640.jpg' class='card-img-top'></img>
-              </div>
-              <div class='card-body'>
-                <h5 class='card-title'>recipe name</h5>
-                <p class='card-text'>description</p>
-                <div class='author'>
-                  <img class='avatar' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png' alt='authorAvatar '></img>
-                  By 
-                  <p> authorName</p>
-                </div>
-              </div>
-              <div class='card-footer'>
-                <div class='card-footer-content'>
-                  <ul>
-                    <li title='difficulty'>difficulty </li>
-                    <li title='yeild'><FaTable/> Yeild </li>
-                    <li title='number of people'><FaUsers/> number ppl </li>
-                    <li title='time'><FaClock/> time</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class='col-md-4'>
-            <div class='card-text-center'>
-            <div class='img-container'>
-                <img src='https://caresspet.com/wp-content/uploads/2018/10/s15-1024x640.jpg' class='card-img-top'></img>
-              </div>
-              <div class='card-body'>
-                <h5 class='card-title'>recipe name</h5>
-                <p class='card-text'>description</p>
-                <div class='author'>
-                  <img class='avatar' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png' alt='authorAvatar '></img>
-                  By 
-                  <p> authorName</p>
-                </div>
-              </div>
-              <div class='card-footer'>
-                <div class='card-footer-content'>
-                  <ul>
-                    <li title='difficulty'>difficulty </li>
-                    <li title='yeild'><FaTable/> Yeild </li>
-                    <li title='number of people'><FaUsers/> number ppl </li>
-                    <li title='time'><FaClock/> time</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class='col-md-4'>
-            <div class='card-text-center'>
-            <div class='img-container'>
-                <img src='https://caresspet.com/wp-content/uploads/2018/10/s15-1024x640.jpg' class='card-img-top'></img>
-              </div>
-              <div class='card-body'>
-                <h5 class='card-title'>recipe name</h5>
-                <p class='card-text'>description</p>
-                <div class='author'>
-                  <img class='avatar' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png' alt='authorAvatar '></img>
-                  By 
-                  <p> authorName</p>
-                </div>
-              </div>
-              <div class='card-footer'>
-                <div class='card-footer-content'>
-                  <ul>
-                    <li title='difficulty'>difficulty </li>
-                    <li title='yeild'><FaTable/> Yeild </li>
-                    <li title='number of people'><FaUsers/> number ppl </li>
-                    <li title='time'><FaClock/> time</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
+        </div>
+      </div> 
         </div>
           
       </div>
@@ -275,6 +68,7 @@ const RecipeItems = (props) =>{
        
 
   );
+        
 }
 
 export default RecipeItems;
