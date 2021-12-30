cec30c22629187d3d7c2144e5bf4301c05fbedb7
ntttrung <trung.ntt1210@gmail.com>
2021-12-30 20:19:20 +0700

    fix key leak

--- server/src/emails/account.js
+++ server/src/emails/account.js
@@ -1,35 +1,35 @@
 const sgMail = require('@sendgrid/mail')
 
-const sendgridAPIKey = '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'
+const sendgridAPIKey = 
 
 sgMail.setApiKey(sendgridAPIKey)
 
-// sgMail.send({
-//             to: 'trung.ntt1210@gmail.com',
-//             from: 'recipeapp.group10@gmail.com',
-//             subject: 'Thanks for joining in!',
-//             text: "test lai"
-//         })
+sgMail.send({
+            to: 'trung.ntt1210@gmail.com',
+            from: 'recipeapp.group10@gmail.com',
+            subject: 'Thanks for joining in!',
+            text: "test lai"
+        })
 
-const sendWelcomeEmail = (email, name) => {
-    sgMail.send({
-        to: email,
-        from: 'recipeapp.group10@gmail.com',
-        subject: 'Thanks for joining in!',
-        text: `Welcome ${name} to our food website.`
-    })
-}
+// const sendWelcomeEmail = (email, name) => {
+//     sgMail.send({
+//         to: email,
+//         from: 'recipeapp.group10@gmail.com',
+//         subject: 'Thanks for joining in!',
+//         text: `Welcome ${name} to our food website.`
+//     })
+// }
 
-const sendResetpassEmail = (email, name, pass) => {
-    sgMail.send({
-        to: email,
-        from: 'recipeapp.group10@gmail.com',
-        subject: 'Reset Password',
-        text: `Hello, ${name}. Your password has been changed to: ${pass}`
-    })
-}
+// const sendResetpassEmail = (email, name, pass) => {
+//     sgMail.send({
+//         to: email,
+//         from: 'recipeapp.group10@gmail.com',
+//         subject: 'Reset Password',
+//         text: `Hello, ${name}. Your password has been changed to: ${pass}`
+//     })
+// }
 
-module.exports = {
-    sendWelcomeEmail,
-    sendResetpassEmail
-}
+// module.exports = {
+//     sendWelcomeEmail,
+//     sendResetpassEmail
+// }
