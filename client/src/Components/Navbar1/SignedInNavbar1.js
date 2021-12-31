3dc864e747c234e0fd8431ae8d5fc56b8acd56fc
HungN <nguyenhunghnuekt@gmail.com>
2021-12-31 13:51:25 +0700

    add logout

--- client/src/Components/Navbar1/SignedInNavbar1.js
+++ client/src/Components/Navbar1/SignedInNavbar1.js
@@ -5,9 +5,33 @@ import { Container, Nav } from 'react-bootstrap';
 import "./index.css"
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min';
+import axios from 'axios';
+
+const API = "http://localhost:4000";
 
 const SignedInNavbar1 = () => {
-  const username = localStorage.getItem("username")
+  const username = localStorage.getItem("username");
+  const token = localStorage.getItem("token");
+  const axiosInstance = axios.create({
+    baseURL: API,
+    timeout: 10000,
+    headers: {'Authorization': 'Bearer '+token}
+  });
+  const handleLogout = async (user) => {
+    user.preventDefault();
+    console.log(username);
+    console.log(token);
+    try {
+      const res = await axiosInstance.post("/users/logoutAll");
+      let resData = res.data;
+      window.alert(resData);
+      localStorage.clear();
+      window.location.reload();
+    } catch (err) {
+      window.alert("Logout failed!");
+    }
+  };
+
   return (
     <>
  
@@ -19,7 +43,7 @@ const SignedInNavbar1 = () => {
     <Nav className="ms-auto">
       <Nav.Link href="/members">{username}</Nav.Link>
       <Nav.Link href="/submit">New Recipe</Nav.Link>
-      <Nav.Link href="/signout">Sign out</Nav.Link>
+      <Nav.Link href="/" onClick={handleLogout}>Sign out</Nav.Link>
     </Nav>
     </Container>
   </Navbar>
