80bc18174fab087a85430dbce7292c199bfc0f48
thu1404 <thulk21cht@gmail.com>
2022-01-01 22:19:04 +0700

    add recipe list to homepage

--- client/src/Pages/HomePage/index.js
+++ client/src/Pages/HomePage/index.js
@@ -1,100 +1,57 @@
-import {React,useState,useEffect} from "react";
-import Navbar1 from '../../Components/Navbar1';
-import SlideShow from '../../Components/SlideShow';
-import Navbar2 from '../../Components/Navbar2';
-import QuickFilter from '../../Components/QuickFilter';
+import { React, useState, useEffect } from "react";
+import Navbar1 from "../../Components/Navbar1";
+import SlideShow from "../../Components/SlideShow";
+import Navbar2 from "../../Components/Navbar2";
+import QuickFilter from "../../Components/QuickFilter";
 import RecipeItems from "../Recipe/recipeItems";
 import Footer from "../../Components/Footer";
 import axios from "axios";
 
-const API = "http://localhost:4000";
+const API = "http://localhost:4000/";
 
 const HomePage = () => {
-  const [topRated, setTopRated] = useState([]);
   const [mostFavorite, setMostFavorite] = useState([]);
   const [latestRecipes, setLatestRecipes] = useState([]);
   const [topAuthors, setTopAuthors] = useState([]);
 
-  // useEffect(()=>{
-  //     const getData = async () =>{
-  //         const res1 = await axios.get(API+'/top_rated');
-  //         setTopRated(res1.data);
-  //         const res2 = await axios.get(API+'/most_favorite');
-  //         setMostFavorite(res2.data);
-  //         const res3 = await axios.get(API+'/latest_recipes');
-  //         setLatestRecipes(res3.data);
-  //         const res4 = await axios.get(API+'/top_authors');
-  //         setTopAuthors(res4.data);
-  //     };
-  //     getData();
-  // }
-  // ,[])
-  const recipes = [
-    {
-      _id: "0",
-      title: "pho",
-      description: "test desciption",
-      author: "author name",
-      difficulty: "3",
-      time: "30",
-    },
-    {
-      _id: "0",
-      title: "pho",
-      description: "test desciption",
-      author: "author name",
-      difficulty: "3",
-      time: "30",
-    },
-    {
-      _id: "0",
-      title: "pho",
-      description: "test desciption",
-      author: "author name",
-      difficulty: "3",
-      time: "30",
-    },
-    {
-      _id: "0",
-      title: "pho",
-      description: "test desciption",
-      author: "author name",
-      difficulty: "3",
-      time: "30",
-    },
-  ];
+  useEffect(() => {
+    const getMostFavorite = async () => {
+      const res = await axios.get(
+        API + "recipes?sortBy=numOfFavourtie:desc&limit=5"
+      );
+      setMostFavorite(res.data);
+    };
+    const getLatest = async () => {
+      const res = await axios.get(
+        API + "recipes?sortBy=createdAt:desc&limit=5"
+      );
+      setLatestRecipes(res.data);
+    };
+    const getAuthors = async () => {
+      const res = await axios.get(
+        API + "members?sortBy=numOfFavourite:desc&limit=5"
+      );
+      setTopAuthors(res.data);
+    };
+    getMostFavorite();
+    getLatest();
+    getAuthors();
+  }, []);
+
   return (
-    <div>
+    <div className="home">
       <Navbar1 />
       <SlideShow />
       <Navbar2 />
       <QuickFilter />
+      {topAuthors.map((item) => console.log(item))}
       <section id="recipeItems">
-        <div class="container">
-          <div class="title">
-            <h1>Recipes </h1>
-          </div>
-          <div class="row">
-            {recipes.map((item) => (
-              <RecipeItems
-                id={item._id}
-                title={item.title}
-                description={item.description}
-                author={item.author.name}
-                difficulty={item.difficulty}
-                time={item.cookTime}
-              />
-            ))}
-          </div>
-        </div>
-      </section>
-      <section id="recipeItems">
-        <div class="container">
-          <div class="title">
+        <div className="container">
+          <div className="title">
             <h1>Top Rated Recipes </h1>
           </div>
-          <div class="row">
-            {recipes.map((item) => (
+          <div className="row">
+            {mostFavorite.map((item) => (
               <RecipeItems
                 id={item._id}
                 title={item.title}
@@ -108,12 +65,12 @@ const HomePage = () => {
         </div>
       </section>
       <section id="recipeItems">
-        <div class="container">
-          <div class="title">
+        <div className="container">
+          <div className="title">
             <h1>Latest Recipes </h1>
           </div>
-          <div class="row">
-            {recipes.map((item) => (
+          <div className="row">
+            {latestRecipes.map((item) => (
               <RecipeItems
                 id={item._id}
                 title={item.title}
@@ -126,6 +83,7 @@ const HomePage = () => {
           </div>
         </div>
       </section>
+
       <Footer />
     </div>
   );
